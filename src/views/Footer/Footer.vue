4187251432941bfb87d3ddb41af81947388bcf1d
Lesya <lesyaaprok@gmail.com>
2021-10-12 16:01:49 +0300

    feat: add footer

---
+++ src/views/Footer/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+    <v-footer color="cyan darken-1" dark>
+      <v-col class="white--text text-center font-weight-bold">
+        Weather, 2021
+      </v-col>
+    </v-footer>
+  </footer>
+</template>  
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Footer'
+})
+</script>

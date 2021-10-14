706cb816ef4f89b836f1156537a2a1b21485574b
Lesya <lesyaaprok@gmail.com>
2021-10-14 14:17:06 +0300

    feat: add header and footer

--- src/views/Footer/Footer.vue
+++ src/views/Footer/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-footer color="cyan darken-1" dark>
-      <v-col class="white--text text-center font-weight-bold">
+    <v-footer class="footer" color="cyan darken-1" dark>
+      <v-col class="footer__info white--text text-center font-weight-bold">
         Weather, 2021
       </v-col>
     </v-footer>
--- src/views/Header/Header.vue
+++ src/views/Header/Header.vue
@@ -1,89 +1,97 @@
 <template>
-  <header class="header">
-    <v-app-bar app color="cyan darken-1" dark>
-      <v-col class="d-flex white--text font-weight-bold">
-        <v-app-bar-nav-icon
-          @click="drawer = !drawer"
-          class="hidden-md-and-up"
-        ></v-app-bar-nav-icon>
-        <v-app-bar-title class="d-flex align-center pl-3"
-          >Weather</v-app-bar-title
+  <v-app-bar class="header" app color="cyan darken-1" dark>
+    <v-col class="header__column d-flex white--text font-weight-bold">
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        class="header__hide-menu-button hidden-md-and-up"
+      ></v-app-bar-nav-icon>
+      <v-app-bar-title class="header__title d-flex align-center pl-3"
+        >Weather</v-app-bar-title
+      >
+      <v-spacer></v-spacer>
+      <v-toolbar-items
+        class="header__nav hidden-sm-and-down"
+        color="blue"
+        height="100%"
+      >
+        <router-link
+          class="nav__link white--text text-decoration-none"
+          v-for="item in menuItems"
+          :key="item.id"
+          :to="item.path"
         >
-        <v-spacer></v-spacer>
-        <v-toolbar-items color="blue" class="hidden-sm-and-down" height="100%">
-          <router-link
-            v-for="item in menuItems"
-            :key="item.id"
-            class="white--text text-decoration-none d-block"
-            :to="item.path"
-          >
-            <v-btn depressed color="cyan darken-1" class="white"
-            >
-              {{ item.name }}
-            </v-btn>
-          </router-link>
-        </v-toolbar-items>
-      </v-col>
-    </v-app-bar>
+          {{ item.name }}
+        </router-link>
+      </v-toolbar-items>
+    </v-col>
     <v-navigation-drawer
+      class="header__navigation-drawer"
       v-model="drawer"
       absolute
       left
+      height="100vh"
       temporary
       dark
       color="cyan darken-1"
       overlay-color="black"
+      touch-action="auto"
     >
-      <v-list nav>
+      <v-list nav class="navigation-drawer__items">
         <v-list-item-group active-class="white--text text--accent-4">
-          <v-list-item v-for="item in menuItems" :key="item.id">
-            {{ item.name }}
-          </v-list-item>
+          <router-link
+            :to="item.path"
+            v-for="item in menuItems"
+            :key="item.id"
+            class="
+              navigation-drawer__item
+              white--text
+              text-decoration-none
+              d-block
+            "
+          >
+            <v-list-item>
+              {{ item.name }}
+            </v-list-item>
+          </router-link>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-  </header>
+  </v-app-bar>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import menuItems from "./constants";
 
 export default Vue.extend({
   name: "Header",
   data() {
     return {
       drawer: false,
-      menuItems: [
-        {
-          id: 1,
-          name: "Current weather",
-          path: "/",
-        },
-        {
-          id: 2,
-          name: "Week",
-          path: "/week",
-        },
-        {
-          id: 3,
-          name: "Month",
-          path: "/month",
-        },
-        {
-          id: 4,
-          name: "Maps",
-          path: "/maps",
-        },
-      ],
+      menuItems,
     };
   },
 });
 </script>
 <style lang="scss" scoped>
-.v-btn {
+.nav__link {
+  height: 36px;
+  min-width: 64px;
+  padding: 0 16px;
+  align-items: center;
+  border-radius: 4px;
+  display: inline-flex;
+  flex: 0 0 auto;
+  font-weight: 500;
+  letter-spacing: 0.0892857143em;
+  justify-content: center;
+  outline: 0;
+  position: relative;
+  text-decoration: none;
+  text-indent: 0.0892857143em;
+  text-transform: uppercase;
   transition: all 0.4s ease-in-out;
   &:hover {
-    padding: 25px 0;
     background-color: #00acc1;
     border-radius: 5px;
     box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),

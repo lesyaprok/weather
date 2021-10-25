78af4edb47d97d00bcbddf93dbe3ebd1ba0d4341
Lesya <lesyaaprok@gmail.com>
2021-10-25 17:01:32 +0300

    feat: modify hourly forecast module

--- src/modules/HourlyForecastModule/HourlyForecastModule.vue
+++ src/modules/HourlyForecastModule/HourlyForecastModule.vue
@@ -1,9 +1,33 @@
 <template>
-  <div></div>
+  <section class="hourly-forecast">
+    <v-slide-group show-arrays class="hourly-forecast__cards pa-4 my-10">
+      <v-card
+        class="cards__item d-flex justify-center align-center flex-column px-4 pb-4"
+        v-for="item in hourlyData"
+        :key="item.id"
+      >
+        <v-card-title class="item__hour">{{
+          `${new Date(item.timestamp * 1000).getHours()}:00`
+        }}</v-card-title>
+        <v-img
+          class="item__img"
+          :src="require(`../../assets/weatherIcons/${item.icon || '01d'}.png`)"
+          width="40"
+        >
+        </v-img>
+        <v-card-text class="item__temp" align="center">{{
+          item.temperature >= 0
+            ? `+${Math.round(item.temperature)}`
+            : Math.round(item.temperature)
+        }}</v-card-text>
+      </v-card>
+    </v-slide-group>
+  </section>
 </template>
 <script lang="ts">
 import Vue from 'vue';
 import { mapGetters, mapActions } from 'vuex';
+import { HourlyWeatherData, Coordinates } from '../../services/types';
 
 export default Vue.extend({
   name: 'HourlyForecastModule',
@@ -12,8 +36,11 @@ export default Vue.extend({
       getCoordinates: 'currentWeatherModule/getCoordinates',
       getHourlyData: 'hourlyForecastModule/getHourlyData',
     }),
-    coordinates() {
+    coordinates(): Coordinates {
       return this.getCoordinates;
+    },
+    hourlyData(): HourlyWeatherData {
+      return this.getHourlyData;
     },
   },
   methods: {
